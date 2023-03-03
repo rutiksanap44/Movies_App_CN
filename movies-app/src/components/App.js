b2e2f3043b9ac270ac9facb2bc9756695ec44bec
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-03-03 19:10:54 +0530

    basic layout completed

--- movies-app/src/components/App.js
+++ movies-app/src/components/App.js
@@ -1,9 +1,24 @@
-
+import React from 'react';
+import Navbar from './Navbar';
+import MovieCard from './Moviecard';
+import { data } from '../data';
 
 function App() {
   return (
     <div className="App">
-      My Movies
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+          </div>
+
+          <div className="list">
+            {data.map(movie => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+      </div>
     </div>
   );
 }
